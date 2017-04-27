d25a43e13fb44555bba639951a0acc293064ffef
Henry Paulino <henrypl360@gmail.com>
2017-04-27 16:36:22 -0400

    configuration of project

---
+++ app/main.ios.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import {
+  StyleSheet,
+  View,
+} from 'react-native'
+
+export default class Spout extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+})
