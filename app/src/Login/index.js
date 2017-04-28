2d7de753cafce1ee5e2c3f639d1d1ea0f5285c5f
Henry Paulino <henrypl360@gmail.com>
2017-04-27 22:07:12 -0400

    add login styles

--- app/src/Login/index.js
+++ app/src/Login/index.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react'
-import { View, StyleSheet, Image } from 'react-native'
+import { View, StyleSheet, Image, TextInput, Text, TouchableHighlight } from 'react-native'
 
 export default class Login extends Component {
   render() {
     return (
-      // <View style={styles.container}>
-      // </View>
       <Image source={require('../assets/login-background.png')} style={styles.container}>
+        <View>
+          <Text style={styles.loginText}>Login</Text>
+        </View>
+
+        <View style={styles.inputContainer}>
+          <TextInput style={styles.input} autoCapitalize="none" keyboardType="email-address" />
+        </View>
+
+        <View style={styles.inputContainer}>
+          <TextInput style={styles.input} autoCapitalize="none" secureTextEntry />
+        </View>
+
+        <View style={styles.forgotContainer}>
+          <TouchableHighlight>
+            <Text style={styles.forgotText}>Forgot your password?</Text>
+          </TouchableHighlight>
+        </View>
+
+        <View style={{ alignItems: 'center', marginTop: 40 }}>
+          <TouchableHighlight style={styles.loginBtn}>
+            <Text style={styles.loginBtnText}>LOGIN</Text>
+          </TouchableHighlight>
+        </View>
       </Image>
     )
   }
@@ -18,5 +39,42 @@ const styles = StyleSheet.create({
     resizeMode: 'stretch',
     width: null,
     height: null,
+    justifyContent: 'center',
+    paddingHorizontal: 50,
+  },
+  input: {
+    width: 266,
+    height: 34,
+    color: '#fff',
+  },
+  inputContainer: {
+    borderBottomColor: '#fff',
+    borderBottomWidth: 1,
+    height: 34,
+    marginTop: 30,
+  },
+  loginText: {
+    backgroundColor: 'transparent',
+    color: '#fff',
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+  forgotContainer: {
+    alignItems: 'flex-end',
+    marginTop: 15,
+  },
+  forgotText: {
+    color: '#fff',
+    fontSize: 14,
+  },
+  loginBtn: {
+    borderColor: '#fff',
+    borderWidth: 1,
+    paddingHorizontal: 80,
+    paddingVertical: 10,
+  },
+  loginBtnText: {
+    color: '#fff',
+    fontSize: 18,
   },
 })
